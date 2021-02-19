a88973828c775bd51fe1d869b5b961870c90d7de
Maya Pasiliao <mayapasiliao@gmail.com>
2021-02-18 23:15:25 -0500

    Removed extra print statement, added gif

--- flixster/flixster/MoviesViewController.swift
+++ flixster/flixster/MoviesViewController.swift
@@ -38,8 +38,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               self.movies = dataDictionary["results"] as! [[String:Any]]
             
               self.tableView.reloadData()
-            
-              print(dataDictionary)
 
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
